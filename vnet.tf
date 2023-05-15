ccc5934c931dfc50b1ca1c8701fe1ebebe41fa98
cloudfyy <cloudfyy@hotmail.com>
2023-05-16 05:59:29 +0800

    fix error: cannot create http listener

--- vnet.tf
+++ vnet.tf
@@ -37,3 +37,24 @@ resource "azurerm_subnet" "appgwsubnet" {
   private_endpoint_network_policies_enabled       =true
 }
 
+
+
+/*resource "azurerm_network_security_group" "nsg" {
+  name                = "Allow-65200-65535-For-ApplicationGateway"
+  location            = local.resource_group.location
+  resource_group_name = local.resource_group.name
+
+  security_rule {
+    name                       = "Allow-65200-65535-For-ApplicationGateway"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+}*/
+
